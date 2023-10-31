20bf184d4fabd35a3903bb6d80504c7c96ddbdc4
cosmiceye33 <zanerobert2004@outlook.com>
2023-10-31 17:23:42 +1000

    Create Guitar class with methods

---
+++ prac_06/guitar.py
@@ -0,0 +1,26 @@
+"""
+Guitar Program
+start: 5:10
+Estimated:
+Actual:
+"""
+CURRENT_YEAR = 2023
+VINTAGE_AGE = 50
+class Guitar:
+    """Guitar class"""
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Returns a string of guitar"""
+        return f"{self.name} {self.year} : {self.cost}"
+    def get_age(self,):
+        """Get the age of a guitar"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine is a guitar is vintage"""
+        return self.get_age() >= VINTAGE_AGE
