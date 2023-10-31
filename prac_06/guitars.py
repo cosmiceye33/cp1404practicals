7c39350e8c45eaa93bf184e0dc6ac9a098af7149
Zane <zanerobert2004@outlook.com>
2023-10-31 18:25:10 +1000

    Add code to branch

---
+++ prac_06/guitars.py
@@ -0,0 +1,31 @@
+"""
+Guitar Program
+Estimated:1h 30m
+Actual:1h 3m
+"""
+
+from guitar import Guitar
+from operator import itemgetter
+
+GREETING = "My Guitars!"
+
+guitars = []
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    new_guitar = Guitar(name, year, cost)
+    guitars.append(new_guitar)
+    print(f"{new_guitar} added.")
+    name = input("Name: ")
+
+guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+if guitars:
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars):
+        vintage = "" if guitar.is_vintage() else "(vintage)"
+        print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:10,.2f} {vintage}")
+else:
+    print("Get a guitar!")
