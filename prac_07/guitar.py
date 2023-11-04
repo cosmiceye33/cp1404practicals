2e038c475460b0545e36026afe441e1a9588a95e
Zane <zanerobert2004@outlook.com>
2023-11-04 13:43:35 +1000

    Write less than method

---
+++ prac_07/guitar.py
@@ -0,0 +1,31 @@
+"""
+Guitar Program
+Estimated:1h 30m
+Actual:1h 3m
+"""
+CURRENT_YEAR = 2023
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """Guitar class"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Returns a string of guitar"""
+        return f"{self.name} {self.year} : {self.cost}"
+
+    def get_age(self):
+        """Get the age of a guitar"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine is a guitar is vintage"""
+        return self.get_age() >= VINTAGE_AGE
+    def __lt__(self, other):
+        return self.year < other.year
