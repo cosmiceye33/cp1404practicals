8155268850d1abf3504c9d7e06a8ed587a8387dc
Zane <zanerobert2004@outlook.com>
2023-11-05 10:13:57 +1000

    Refactor code into functions

--- prac_07/project_manager.py
+++ prac_07/project_manager.py
@@ -12,6 +12,111 @@ projects = []
 
 
 def main():
+    load_projects()
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "l":
+            load_new_projects()
+        elif choice == "d":
+            display_projects()
+        elif choice == "a":
+            add_new_project()
+        elif choice == "s":
+            save_projects()
+        elif choice == "f":
+            display_projects_by_date()
+        elif choice == "u":
+            update_project_details()
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").lower()
+    print("Thank you for using custom-built project management software.")
+    save_ending_projects()
+
+
+def save_ending_projects():
+    with open(FILENAME, 'w') as out_file:
+        for project in projects:
+            print(project, file=out_file, end="\n")
+
+
+def update_project_details():
+    for i, project in enumerate(projects):
+        print(f"{i} {project}")
+    project_choice = int(input("Project choice: "))
+    print(projects[project_choice])
+    percentage = int(input("New percentage: "))
+    priority = int(input("Priority: "))
+    projects[project_choice] = Project(projects[project_choice].name, projects[project_choice].start_date, priority,
+                                       projects[project_choice].cost_estimate, percentage)
+
+
+def display_projects_by_date():
+    date_string = input("Show projects that start after date (dd/mm/yyyy): ")  # e.g., "30/9/2022"
+    date_string = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+    for project in projects:
+        date = datetime.datetime.strptime(project.start_date, "%d/%m/%Y").date()
+        if date > date_string:
+            print(project)
+
+
+def save_projects():
+    filename = input("Filename: ")
+    with open(filename, 'w') as out_file:
+        for project in projects:
+            print(project, file=out_file, end="\n")
+    print(f"Projects saved to {out_file}")
+
+
+def add_new_project():
+    project_name = get_project_details("Name: ")
+    date_string = get_project_details("Start Date (d/m/yyyy): ")
+    priority = int(get_project_details("Priority: "))
+    cost = float(get_project_details("Cost estimate: $"))
+    completion = int(get_project_details("Completion rate: "))
+    projects.append(Project(project_name, date_string, priority, cost, completion))
+
+
+def display_projects():
+    complete_projects, incomplete_projects = determine_if_complete()
+    print("Complete Project:")
+    for project in complete_projects:
+        print(f" {project}")
+    print("Incomplete Project:")
+    incomplete_projects.sort()
+    for project in incomplete_projects:
+        print(f" {project}")
+
+
+def determine_if_complete():
+    complete_projects = []
+    incomplete_projects = []
+    for project in projects:
+        if project.is_complete():
+            complete_projects.append(project)
+        else:
+            incomplete_projects.append(project)
+    complete_projects.sort()
+    return complete_projects, incomplete_projects
+
+
+def load_new_projects():
+    projects.clear()
+    new_projects = input("Filename: ")
+    with open(new_projects, 'r') as in_file:
+        in_file.readline()
+        for line in in_file:
+            line = line.strip()
+            parts = line.split("\t")
+            parts[2] = int(parts[2])
+            parts[3] = float(parts[3])
+            parts[4] = int(parts[4])
+            projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
+
+
+def load_projects():
     with open(FILENAME, 'r') as in_file:
         in_file.readline()
         for line in in_file:
@@ -21,70 +126,6 @@ def main():
             parts[3] = float(parts[3])
             parts[4] = int(parts[4])
             projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
-    print(MENU)
-    choice = input(">>> ").lower()
-    while choice != "q":
-        if choice == "l":
-            projects.clear()
-            new_projects = input("Filename: ")
-            with open(new_projects, 'r') as in_file:
-                in_file.readline()
-                for line in in_file:
-                    line = line.strip()
-                    parts = line.split("\t")
-                    parts[2] = int(parts[2])
-                    parts[3] = float(parts[3])
-                    parts[4] = int(parts[4])
-                    projects.append(Project(parts[0], parts[1], parts[2], parts[3], parts[4]))
-            print(projects)
-        elif choice == "d":
-            complete_projects = []
-            incomplete_projects = []
-            for project in projects:
-                if project.is_complete():
-                    complete_projects.append(project)
-                else:
-                    incomplete_projects.append(project)
-            complete_projects.sort()
-            print("Complete Project:")
-            for project in complete_projects:
-                print(f" {project}")
-            print("Incomplete Project:")  # 2 save function 2 load function
-            incomplete_projects.sort()
-            for project in incomplete_projects:
-                print(f" {project}")
-        elif choice == "a":
-            project_name = get_project_details("Name: ")
-            date_string = get_project_details("Start Date (d/m/yyyy): ")
-            priority = int(get_project_details("Priority: "))
-            cost = float(get_project_details("Cost estimate: $"))
-            completion = int(get_project_details("Completion rate: "))
-            projects.append(Project(project_name, date_string, priority, cost, completion))
-        elif choice == "s":
-            filename = input("Filename: ")
-            with open(filename, 'w') as out_file:
-                for project in projects:
-                    print(project, file=out_file, end="\n")
-            print(f"Projects saved to {out_file}")
-        elif choice == "f":
-            date_string = input("Show projects that start after date (dd/mm/yyyy): ")  # e.g., "30/9/2022"
-            date_string = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-            for project in projects:
-                date = datetime.datetime.strptime(project.start_date, "%d/%m/%Y").date()
-                if date > date_string:
-                    print(project)
-        elif choice == "u":
-            for i, project in enumerate(projects):
-                print(f"{i} {project}")
-            project_choice = int(input("Project choice: "))
-            print(projects[project_choice])
-            percentage = int(input("New percentage: "))
-            priority = int(input("Priority: "))
-            projects[project_choice] = Project(projects[project_choice].name, projects[project_choice].start_date, priority, projects[project_choice].cost_estimate, percentage)
-        else:
-            print("Invalid menu choice")
-        print(MENU)
-        choice = input(">>> ").lower()
 
 
 def get_project_details(display_string):
@@ -96,7 +137,3 @@ def get_project_details(display_string):
 
 
 main()
-# date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
-# date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-#         print(f"That day is/was {date.strftime('%A')}")
-#         print(date.strftime("%d/%m/%Y")
