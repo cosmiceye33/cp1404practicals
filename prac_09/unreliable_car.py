82a7feac80dd9ad4abb71511878bd9ed3735b433
Zane <zanerobert2004@outlook.com>
2023-11-18 21:39:22 +1000

    Complete some of the question

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,9 +3,10 @@ import random
 
 
 class UnreliableCar(Car):
-    def __init__(self, name, fuel, reliability: float):
-        super().__init__(name, fuel, )
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
         self.reliability = reliability
+
 
     def drive(self, distance):
         if random.randrange(0, 100) < self.reliability:
