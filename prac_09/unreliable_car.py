38e0f4bef573f4054834fcd20ea6ed5778409b42
Zane <zanerobert2004@outlook.com>
2023-11-18 21:06:08 +1000

    Complete reliability class

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,14 @@
+from car import Car
+import random
+
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability: float):
+        super().__init__(name, fuel, )
+        self.reliability = reliability
+
+    def drive(self, distance):
+        if random.randrange(0, 100) < self.reliability:
+            super().drive(distance)
+        else:
+            return 0
